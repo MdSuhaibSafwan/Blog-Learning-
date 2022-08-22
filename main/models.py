ef97f1940b5cff1fde2b1110efc8f947802149a1
MD Suhaib Safwan <68965547+MdSuhaibSafwan@users.noreply.github.com>
2022-08-22 22:22:00 +0600

    created models for blog

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+# from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Blog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) # one to many
+    """
+    ONE BLOG WILL HAVE ONE USER ONLY,
+    BUT
+    ONE USER WILL HAVE MULTIpLE BLOGS.
+    """
+    content = models.TextField()
+    title = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
